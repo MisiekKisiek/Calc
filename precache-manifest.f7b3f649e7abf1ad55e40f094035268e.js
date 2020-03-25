1e50c3b10bfead815a6d7faaa89aa2b4ab19599a
MisiekKisiek <skrzypkka@gmail.com>
2020-03-25 15:57:16 +0100

    Updates

---
+++ precache-manifest.f7b3f649e7abf1ad55e40f094035268e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af1ae9f8d2772bf5ed9554d1636ae65c",
+    "url": "/Calc/index.html"
+  },
+  {
+    "revision": "fe7cf00912ed605f8f5d",
+    "url": "/Calc/static/css/main.bfd3b857.chunk.css"
+  },
+  {
+    "revision": "5a18eaa65f7ed1bfc467",
+    "url": "/Calc/static/js/2.db20bfc8.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/Calc/static/js/2.db20bfc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe7cf00912ed605f8f5d",
+    "url": "/Calc/static/js/main.3545bb5d.chunk.js"
+  },
+  {
+    "revision": "32651ab0d23f522ba11d",
+    "url": "/Calc/static/js/runtime-main.86faab72.js"
+  }
+]);
